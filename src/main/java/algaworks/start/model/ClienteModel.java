7c4c8c1c3a3e29d3eb7f258ec60807c798351e74
MatheusTh1 <matheushenriquesilva.th@gmail.com>
2023-02-07 08:47:20 -0300

    criado model cliente e método notificador

---
+++ src/main/java/algaworks/start/model/ClienteModel.java
@@ -0,0 +1,13 @@
+package algaworks.start.model;
+
+import lombok.Data;
+
+@Data
+public class ClienteModel {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private boolean ativo;
+
+}
